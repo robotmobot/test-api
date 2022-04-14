67a5fae8c6b2f85d66ad1a60d0af7a0ade327c5f
robotmobot <ozan.koftegul@gmail.com>
2022-04-14 12:06:05 +0300

    added routes

--- server.go
+++ server.go
@@ -1,15 +1,25 @@
 package main
 
 import (
-	"net/http"
+	"test-api/handler"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	//routes
+	e.GET("/products/:id", handler.GetProduct)
+	e.GET("/products", handler.GetAllProducts)
+	e.POST("/products", handler.CreateProduct)
+	e.PUT("/products/:id", handler.UpdateProduct)
+	e.DELETE("/products/:id", handler.DeleteProduct)
+
 	e.Logger.Fatal(e.Start(":1323"))
+
 }
