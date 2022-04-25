890b4f269853e1bebe8c22b2beab583756501487
Ahmet Yilmaz <ahmet.yilmaz@birlesikodeme.com>
2022-04-25 13:58:12 +0300

    Add new fiels for product model

--- model/products.go
+++ model/products.go
@@ -1,6 +1,9 @@
 package model
 
 type Product struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
+	ID         int     `json:"id"`
+	Name       string  `json:"name"`
+	Detail     string  `json:"detail"`
+	Price      float64 `json:"price"`
+	IsCampaign bool    `json:"is_campaign"`
 }
