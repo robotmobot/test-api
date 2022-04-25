1ff490b3996c9b4c1992b94260d48cf17598e350
robotmobot <ozan.koftegul@gmail.com>
2022-04-25 14:56:18 +0300

    Add config for db connection string

--- config/db.go
+++ config/db.go
@@ -1,26 +1,41 @@
 package config
 
-import "fmt"
-
-const (
-	DBUser     = "user"
-	DBPassword = "password"
-	DBName     = "test_data"
-	DBHost     = "testdb_postgres"
-	DBPort     = "5432"
-	DBType     = "postgres"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
 )
 
+type ConnString struct {
+	DBUser     string `json:"DBUser"`
+	DBPassword string `json:"DBPassword"`
+	DBName     string `json:"DBName"`
+	DBHost     string `json:"DBHost"`
+	DBPort     string `json:"DBPort"`
+	DBType     string `json:"DBType"`
+}
+
+func GetConnString() ConnString {
+	var cfg ConnString
+
+	cfgFile, _ := os.Open("config/connString.json")
+	defer cfgFile.Close()
+
+	jsonParser := json.NewDecoder(cfgFile)
+	jsonParser.Decode(&cfg)
+	return cfg
+}
+
 func GetDBType() string {
-	return DBType
+	return GetConnString().DBType
 }
 
 func GetPostgresConnectionString() string {
 	dataBase := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable",
-		DBHost,
-		DBPort,
-		DBUser,
-		DBName,
-		DBPassword)
+		GetConnString().DBHost,
+		GetConnString().DBPort,
+		GetConnString().DBUser,
+		GetConnString().DBName,
+		GetConnString().DBPassword)
 	return dataBase
 }
